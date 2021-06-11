3f337549dc96973d5945793fb3b26aa359ec1cc6
vihaan1212 <73737597+vihaan1212@users.noreply.github.com>
2021-06-11 20:28:13 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,7 @@
+var player1_name = "";
+var player2_name = "";
+
+function addUser() {
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+}
